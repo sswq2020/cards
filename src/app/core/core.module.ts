6ca67d56032bd2d4a5e4f13cd9ddbd0468e16e7b
sswq2020 <a309108243@qq.com>
2020-02-02 17:00:59 +0800

    新增core和shared模块

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已经存在,不能再次加载');
+    }
+  }
+}
